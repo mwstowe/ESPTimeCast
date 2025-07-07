cbe22a40660a4f6f4b24912980b90a1e4c91fe42
Michael Stowe <mstowe@gmail.com>
2025-07-06 20:15:12 -0700

    Added parseNetatmoJson function prototype

--- ESPTimeCast.ino
+++ ESPTimeCast.ino
@@ -24,6 +24,7 @@ String formatTemperature(float temperature, bool roundToInteger = false);
 void fetchNetatmoIndoorTemperature();
 String getNetatmoToken();
 void forceNetatmoTokenRefresh();
+bool parseNetatmoJson(String &payload, JsonDocument &doc);
 
 #define HARDWARE_TYPE MD_MAX72XX::FC16_HW
 #define MAX_DEVICES 4
